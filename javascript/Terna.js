51a8684f033cae3c1bacb4b0422ce1a888afec4e
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-09-15 21:32:00 +0545

    Update Terna.js

--- javascript/Terna.js
+++ javascript/Terna.js
@@ -1,7 +1,9 @@
+
 const age = 22;
 const output = age>=18 ? "Eligible to vote" : "Not Eligible";
 console.log(output);
 
 // Output: Eligible to vote
 // Explanation: The code checks if the age is 18 or more, and sets the status accordingly.
+
 
